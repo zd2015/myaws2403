ad515f38bf759c5d53be8ca06db63ebed773d68d
Dong Zheng <dongzheng2020@Dongs-MacBook-Air.local>
2024-03-08 19:07:38 -0500

    2 step

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn my React
         </a>
       </header>
     </div>
